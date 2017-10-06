0e8bbf2f9bf9e842d5352bcdb8c2f8f68b59af68
Joerg Spoerl <joerg.spoerl@gmx.de>
2017-10-06 11:35:49 +0200

    create pages folder & components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,16 @@ import {MdButtonModule, MdCheckboxModule, MdTabsModule } from '@angular/material
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { StartComponent } from './pages/start/start.component';
+import { AboutComponent } from './pages/about/about.component';
+import { SettingsComponent } from './pages/settings/settings.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StartComponent,
+    AboutComponent,
+    SettingsComponent
   ],
   imports: [
    BrowserModule,
